36e8d7bb1d8351dbba6f7625d2003e83f110e273
Larry Academy <olanrewajuawoyemi@gmail.com>
2024-09-06 05:33:30 +0100

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,22 +1,27 @@
 #!/usr/bin/node
 
-const req = require('request');
-const movieId = process.argv[2];
-const api_url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+const request = require('request');
 
-req(api_url, async (err, response, body) => {
-  if (err) {
-    console.log(err);
+const req = (arr, i) => {
+  if (i === arr.length) return;
+  request(arr[i], (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(JSON.parse(body).name);
+      req(arr, i + 1);
+    }
+  });
+};
+
+request(
+  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
+  (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      const chars = JSON.parse(body).characters;
+      req(chars, 0);
+    }
   }
-  for (const characterId of JSON.parse(body).characters) {
-    await new Promise((resolve, reject) => {
-      req(characterId, (err, response, body) => {
-        if (err) {
-          reject(err);
-        }
-        console.log(JSON.parse(body).name);
-        resolve();
-      });
-    });
-  }
-});
+);
